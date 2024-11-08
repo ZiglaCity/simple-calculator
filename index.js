6dfbd2540cc07cf0070fd7dfb15cdc5a0caf4821
ZiglaCity <ziglacity@gmail.com>
2024-11-08 14:01:17 +0000

    created the interface

--- index.js
+++
@@ -1,7 +0,0 @@
-echo "# simple-calculator-app" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/ZiglaCity/simple-calculator-app.git
-git push -u origin main
