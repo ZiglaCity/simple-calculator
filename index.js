28a00663bbdbac10fd541531c11e593538930a94
ZiglaCity <ziglacity@gmail.com>
2024-11-08 14:27:35 +0000

    created a separate function for operetors

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function addNumber(num) {
     document.getElementById("result").value += num;
-   }
+}
+  
+
+function addOperator(operator) {
+    document.getElementById("result").value += operator;
+}
   
